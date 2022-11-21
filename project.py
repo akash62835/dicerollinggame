1660f5efffb36d9a4dda696a47ed00fdd4ec9a62
akash62835 <113462148+akash62835@users.noreply.github.com>
2022-11-21 14:12:50 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,21 @@
+import random
+
+print("***************Welcome to DICE GAME!***************")
+
+i = ""
+scr = 0
+
+while i != "n":
+    x = int(input("Enter Number from (1-6): "))
+    y = random.randint(1, 6)
+    if x not in range(1, 6 + 1):
+        print("Number must be between 1 to 6!")
+    else:
+        print("Number enter By the User:",x)
+        print("Number after rolling the Dice:",y)
+        if x == y:
+            scr += 10
+    i = str(input("Do you want to continue? (Y/n): "))
+
+print("Game Exited!")
+print("Your Score:", scr)
